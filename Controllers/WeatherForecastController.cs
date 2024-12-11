80ad08036424809591135c022eb7af1a1bb47dbc
anh.le <anh.le@s3corp.com.vn>
2024-12-11 11:01:27 +0700

    add text

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CI_CD_Example.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Quanh"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
